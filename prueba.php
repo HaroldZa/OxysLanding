d7832621db5fb654ed0d01902fa7ed1aa9fc2cc4
HaroldZa <harold.jzarco@gmail.com>
2023-02-24 12:19:21 -0600

    Landing completa

---
+++ prueba.php
@@ -0,0 +1,12 @@
+<a id="elem1" href="/OxysLanding">link</a>
+
+<script src="./content/js/jquery-1.12.4.min.js"></script>
+
+<script>
+
+$(window).on('popstate', function (e) {
+    document.getElementById("elem1").click(); 
+});
+
+
+</script>
